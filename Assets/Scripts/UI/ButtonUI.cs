0d4cc2bba7e65e67990ed849331a3d7d626b800d
Mauro <mauro.mendozacp@gmail.com>
2021-09-17 01:51:59 -0300

    art menu added

---
+++ Assets/Scripts/UI/ButtonUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class ButtonUI : MonoBehaviour
+{
+    Color originalColor = Color.white;
+    Color pressedColor = Color.red;
+
+    public void PointerEnter(TMP_Text text) 
+    {
+        text.color = pressedColor;
+    }
+
+    public void PointerExit(TMP_Text text)
+    {
+        text.color = originalColor;
+    }
+}
