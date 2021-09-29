55240735fc6beb7428ab015ae37f386826896924
Mauro <mauro.mendozacp@gmail.com>
2021-09-29 12:18:17 -0300

    survivor animation run and dead implemented

---
+++ Assets/Scripts/Gameplay/AnimationClipsRename.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class AnimationClipsRename : MonoBehaviour
+{
+    [SerializeField] AnimationClip clip = null;
+    [SerializeField] string oldName = String.Empty;
+    [SerializeField] string rename = String.Empty;
+
+    private void Awake()
+    {
+        if (clip != null)
+        {
+            for (int i = 0; i < AnimationUtility.GetCurveBindings(clip).Length; i++)
+            {
+                AnimationCurve animCurve = AnimationUtility.GetEditorCurve(clip, AnimationUtility.GetCurveBindings(clip)[i]);
+                EditorCurveBinding CopyCurve = AnimationUtility.GetCurveBindings(clip)[i];
+                CopyCurve.path = CopyCurve.path.Replace(oldName, rename);
+                AnimationUtility.SetEditorCurve(clip, CopyCurve, animCurve);
+                Debug.Log(AnimationUtility.GetCurveBindings(clip)[i].path);
+            }
+        }
+    }
+
+}
